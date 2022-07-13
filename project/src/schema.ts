50c56fc9571c4d6ed61ff31fecdd2a3d0e0920a6
Huan Hoang <huanhoangvb@gmail.com>
2022-07-13 11:20:55 +0700

    add Mutation and its resolver

--- project/src/schema.ts
+++ project/src/schema.ts
@@ -11,6 +11,10 @@ type Link {
   description: String!
   url: String!
 }
+
+type Mutation {
+  postLink(url: String!, description: String!): Link!
+}
 `
 // 1
 type Link = {
@@ -44,6 +48,24 @@ const resolvers = {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
     url: (parent: Link) => parent.url,
+  },
+  Mutation:{
+    postLink: (parent: unknown, args: {description: string, url: string}) =>
+    {
+      // 1
+      let idCount = links.length
+
+      // 2
+      const link: Link = {
+        id: `link-${idCount}`,
+        description: args.description,
+        url: args.url,
+      }
+
+      links.push(link)
+
+      return link
+    },
   },
 }
 
