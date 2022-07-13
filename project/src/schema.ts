74f9dc369205dc55f1cb92ae01db6bfce108a832
Huan Hoang <huanhoangvb@gmail.com>
2022-07-13 08:51:45 +0700

    query test

--- project/src/schema.ts
+++ project/src/schema.ts
@@ -1,14 +1,49 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
 const typeDefinitions = /* GraphQL */ `
-  type Query {
-    hello: String!
-  }
+type Query {
+  info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
 `
+// 1
+type Link = {
+  id: string
+  url: string
+  description: string
+}
+
+// 2
+const links: Link[] = [
+  {
+    id: 'link-0',
+    url: 'https://graphql-yoga.com',
+    description: 'The easiest way of setting up a GraphQL server',
+  },
+  {
+    id: 'link-1',
+    url: 'https://graphql.com',
+    description: 'BOOOHOOO',
+  },
+]
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!',
+    info: () => `This is the API of a Hackernews Clone`,
+    // 3
+    feed: () => links,
+  },
+  // 4
+  Link: {
+    id: (parent: Link) => parent.id,
+    description: (parent: Link) => parent.description,
+    url: (parent: Link) => parent.url,
   },
 }
 
